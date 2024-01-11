290fc4aaad96764e0af5f9022a0c16e0f8a928e8
will-tran8 <willtran787@gmail.com>
2024-01-11 10:56:34 -0500

    more inventory

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -9,3 +9,20 @@ func _ready():
 		item_name = "Iron Sword"
 	elif rand_val == 1:
 		item_name = "Tree Branch"
+
+	$TextureRect.texture = load("res://Images/" + item_name + ".png")
+	var stack_size = int(JsonData.item_data[item_name]["StackSize"])
+	item_quantity = randi() % stack_size + 1
+	 
+	if stack_size == 1:
+		$Label.visible = false
+	else:
+		$Label.text = str(item_quantity)
+
+func add_item_quantity(amount_to_add):
+	item_quantity += amount_to_add
+	$Label.text = str(item_quantity)
+
+func decrease_item_quantity(amount_to_remove):
+	item_quantity -= amount_to_remove
+	$Label.text = str(item_quantity)
--- Scripts/json_data.gd
+++ Scripts/json_data.gd
@@ -1,13 +1,12 @@
 extends Node
 
-var item_data: Dictionary
+var item_data : Dictionary
 
 func _ready():
-	item_data = LoadData("res://Data/ItemData.json")
+	item_data = load_data("res://data/ItemData.json")
 	
-
-func LoadData(file):
-	var file_data = FileAccess.get_file_as_string(file)
+func load_data(file_path : String):
+	var file_data = FileAccess.get_file_as_string(file_path)
 	var json_data = JSON.parse_string(file_data)
 	return json_data
 
